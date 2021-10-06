dd86d88b0fd5d2c2b13600f178320b8e4419c362
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-10-06 13:29:03 +0100

    send airtime utility fn

---
+++ utils.py
@@ -0,0 +1,66 @@
+import requests
+import json
+from app import app
+
+def send_request(url, data, method, headers):
+    try:
+        response = requests.request(method, url, headers=headers, data=data)
+        return {
+            'data': response.json(),
+            'status': response.status_code,
+            'error': None
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'data': None,
+            'error': e,
+            'status': None
+        }
+
+def get_token():
+    url = "https://auth.reloadly.com/oauth/token"
+
+    payload = json.dumps({
+    "client_id": app.config['RELOADLY_CLIENT_ID'],
+    "client_secret": app.config['RELOADLY_CLIENT_SECRET'],
+    "grant_type": "client_credentials",
+    "audience": "https://topups.reloadly.com"
+    })
+    headers = {
+    'Content-Type': 'application/json'
+    }
+    response = send_request(url, payload, "POST", headers)
+    if response['data']:
+        return response['data']
+    else:
+        return None
+        
+
+def topup(amt, phone_number, my_number):
+    url = f"{app.config['RELOADLY_API_URL']}/topups"
+    payload = json.dumps({
+    "operatorId": "685",
+    "amount": amt,
+    "useLocalAmount": False,
+    "customIdentifier": "This is example identifier 092",
+    "recipientPhone": {
+        "countryCode": "NG",
+        "number": phone_number
+    },
+    "senderPhone": {
+        "countryCode": "NG",
+        "number": my_number
+    }
+    })
+    access_token = get_token()['access_token'] if get_token() else None
+    if not access_token:
+        print("No access token")
+        exit()
+    headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Accept': 'application/com.reloadly.topups-v1+json',
+    'Content-Type': 'application/json'
+    }
+
+    return send_request(url, payload, "POST", headers)
